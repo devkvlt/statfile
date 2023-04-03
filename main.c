b96716cef726a08968f4d218b54ba2102bac4ba6
devkvlt <devkvlt@pm.me>
2023-04-03 10:33:59 +0000

    Write stat to a file

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/sysctl.h>
 #include <sys/types.h>
 #include <time.h>
 
-int main() {
-    int mib[2];
+int uptime(int *days, int *hours, int *minutes) {
+    int            mib[2];
     struct timeval boottime;
-    size_t size = sizeof(boottime);
-    time_t now;
-    double uptime;
+    size_t         size = sizeof(boottime);
+    time_t         now;
+    double         uptime;
 
     // Set up sysctl MIB array
     mib[0] = CTL_KERN;
@@ -16,17 +17,30 @@ int main() {
 
     if (sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec) {
         time(&now);
-        uptime = difftime(now, boottime.tv_sec);
+        uptime   = difftime(now, boottime.tv_sec);
+        *days    = (int)uptime / 86400;
+        *hours   = (int)(uptime / 3600) % 24;
+        *minutes = (int)(uptime / 60) % 60;
 
-        int days = (int)uptime / 86400;
-        int hours = (int)(uptime / 3600) % 24;
-        int minutes = (int)(uptime / 60) % 60;
-
-        printf("%dd %dh %dm\n", days, hours, minutes);
+        return 0;
     } else {
-        printf("Failed to retrieve boot time\n");
+        perror("Failed to retrieve boot time");
         return 1;
     }
+}
 
+int main() {
+    FILE *file = fopen("uptime", "w");
+    if (file == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    int d, h, m;
+    uptime(&d, &h, &m);
+
+    fprintf(file, "%dd %dh %dm\n", d, h, m);
+
+    fclose(file);
     return 0;
 }
